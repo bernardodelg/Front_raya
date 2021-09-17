813e1314e0eaf2ac28793e3dce5a12311216af66
Noe Reyes <noe.reyes@nuvem.mx>
2021-09-17 16:55:53 -0500

    initial commit

---
+++ js/precache-manifest.b46cb7553142b09916bf3a331d6e8ae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb0efdbaea46f5da47c9d84ef96213b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e58086634e90e33b4d4",
+    "url": "/static/css/2.16aca965.chunk.css"
+  },
+  {
+    "revision": "478553d1bc2142ec15eb",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6e58086634e90e33b4d4",
+    "url": "/static/js/2.25cef1c9.chunk.js"
+  },
+  {
+    "revision": "22867ef68f997d163bd38cbb86c264fa",
+    "url": "/static/js/2.25cef1c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "478553d1bc2142ec15eb",
+    "url": "/static/js/main.b4cbd66a.chunk.js"
+  },
+  {
+    "revision": "630520f9f599edaa115f",
+    "url": "/static/js/runtime-main.4b1875d2.js"
+  }
+]);
---
+++ precache-manifest.75f6b6bc61839be2e3bdea9f85cf160a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db24fbd2ba91df60d2deca86c541e624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "955b853bc821d903fe72",
+    "url": "/static/css/2.d6f9ee9b.chunk.css"
+  },
+  {
+    "revision": "b5f9dc96101bba556df4",
+    "url": "/static/css/main.732bba4a.chunk.css"
+  },
+  {
+    "revision": "955b853bc821d903fe72",
+    "url": "/static/js/2.9acd34b2.chunk.js"
+  },
+  {
+    "revision": "efbb41fc9fbc7846992a5b06f866e34b",
+    "url": "/static/js/2.9acd34b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5f9dc96101bba556df4",
+    "url": "/static/js/main.84427131.chunk.js"
+  },
+  {
+    "revision": "630520f9f599edaa115f",
+    "url": "/static/js/runtime-main.4b1875d2.js"
+  }
+]);
